e15c654029fcb598d1c5e514281c3f98d57220d2
Navid <navid.anjum@northsouth.edu>
2022-03-28 18:42:11 +0600

    map reworked

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import Search from "./components/search/Search";
-import { Map } from "react-map-gl";
+
 import "./App.css"
+import Map from "./components/map/Map";
 function App() {
   return (
     <div className="container">
       <Search></Search>
-      <Map
-      initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14
-      }}
-      style={{height: '100vh'}}
-      mapStyle={"mapbox://styles/mapbox/streets-v9"}
-      mapboxAccessToken="pk.eyJ1IjoibmF2aWRuYWJpbCIsImEiOiJjbDE2MzJnMWwwNTlnM2Jtd2Zuc2djdTcyIn0.UIYnpGMIMwPxtpllPNQgxg"
-      />
+      <Map></Map>
     </div>
   );
 }
---
+++ src/components/map/Map.jsx
@@ -0,0 +1,37 @@
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import "./map.css"
+mapboxgl.accessToken = 'pk.eyJ1IjoibmF2aWRuYWJpbCIsImEiOiJjbDE2MzJnMWwwNTlnM2Jtd2Zuc2djdTcyIn0.UIYnpGMIMwPxtpllPNQgxg';
+
+export default function Map() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(90.3929);
+  const [lat, setLat] = useState(23.7607);
+  const [zoom, setZoom] = useState(11);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+
+   useEffect(() => {
+     if (!map.current) return; // wait for map to initialize
+     map.current.on('move', () => {
+       setLng(map.current.getCenter().lng.toFixed(4));
+       setLat(map.current.getCenter().lat.toFixed(4));
+       setZoom(map.current.getZoom().toFixed(2));
+     });
+   });
+
+  return (
+    <div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
+}
